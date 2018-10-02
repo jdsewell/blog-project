f26fe4261710e51c84be8f2906b1992871e9ecf5
Jonathan <jonathandavidsewell@gmail.com>
2018-10-02 10:04:58 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  get 'index' => 'practice#index'
+  get 'about' => 'practice#about'
+  post 'index' => 'practice#about'
+  root 'practice#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
